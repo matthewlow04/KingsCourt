9683fcce726c46a9145b8d1a485069a1abc50c32
Matthew Low <matthew7419@gmail.com>
2024-09-06 17:41:47 -0400

    Initial Commit

---
+++ KingsCourt/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  KingsCourt
+//
+//  Created by Matthew Low on 2024-09-06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ KingsCourt/KingsCourtApp.swift
@@ -0,0 +1,17 @@
+//
+//  KingsCourtApp.swift
+//  KingsCourt
+//
+//  Created by Matthew Low on 2024-09-06.
+//
+
+import SwiftUI
+
+@main
+struct KingsCourtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
